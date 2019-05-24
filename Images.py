aa0b64ca4755de399f324740c51e637c76b6fda5
Omid Esmalebeyg <48869020+omid96@users.noreply.github.com>
2019-05-24 19:37:46 +0430

    Add files via upload

---
+++ Images.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+from PIL import Image
+from io import BytesIO
+import os
+
+def StartSearch():
+    search = input("Enter your phrase: ")
+    params = {"q": search}
+    r = requests.get("http://www.bing.com/images/search", params=params)
+
+    soup = BeautifulSoup(r.text, "html.parser")
+    links = soup.findAll("a", {"class": "thumb"})
+    dir_name = search.replace(" ", "_").lower()
+
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    else:
+        for item in links:
+            try:
+                img_obj = requests.get(item.attrs["href"])
+                print("Getting", item.attrs["href"])
+                title = item.attrs["href"].split("/")[-1]
+                try:
+                    img = Image.open(BytesIO(img_obj.content))
+                    img.save("./" + dir_name + "/" + title, img.format)
+                except:
+                    print("Could not save the image.")
+            except:
+                print("Could not request the image.")
+
+    StartSearch()
+
+StartSearch()
